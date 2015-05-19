d90a95a0ab643949090311e15cb583a5c7210794
msiccdev <msiccdev@hotmail.com>
2015-05-19 05:39:05 +0200

    reupload after rebuilding the whole project.

---
+++ StringEncryptionExtension/StringEncryptionExtension.Shared/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace StringEncryptionExtension
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+
+        private void EncyptionButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            ResultTB.Text = InputTextBox.Text.EncryptString(PSKTB.Text);
+        }
+
+        private void DecryptionButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            InputTextBox.Text = ResultTB.Text;
+
+            ResultTB.Text = InputTextBox.Text.DecryptString(PSKTB.Text);
+        }
+
+        private void GetPSKButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            //this is used for demonstration purposes only
+            //please make sure you are using a truly unique identifier for your app
+            PSKTB.Text = Guid.NewGuid().ToString() + "_" + DateTime.Now.Ticks.ToString();
+            GetPSKButton.IsEnabled = false;
+        }
+
+        private void ClearEncDecButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            InputTextBox.Text = string.Empty;
+            ResultTB.Text = string.Empty;
+        }
+
+    }
+}
---
+++ StringEncryptionExtension/StringEncryptionExtension.Shared/StringEncryptionExtensions.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Text;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+
+namespace StringEncryptionExtension
+{
+    public static class StringEncryptionExtensions
+    {
+
+        static StringEncryptionExtensions()
+        {
+        }
+
+        /// <summary>
+        /// Method to obtain a pre shared key
+        /// </summary>
+        /// <returns>pre shared key string representation</returns>
+        private static string GetPreSharedKey()
+        {
+            //todo: implement your logic for a pre shared key here
+            return null;
+        }
+
+        /// <summary>
+        /// encrypts a string
+        /// </summary>
+        /// <param name="text">string to encrypt</param>
+        /// <param name="psk">pre shared key used to encrypt</param>
+        /// <returns></returns>
+        public static string EncryptString(this string text, string psk = null)
+        {
+            //if no PSK is provided, load it from the PreSharedKey method. 
+            //remember to save this key securely between your apps or provide a key that is unique but available to both platforms!
+            if (string.IsNullOrEmpty(psk))
+            {
+                if (string.IsNullOrEmpty(GetPreSharedKey()))
+                {
+                    throw new NullReferenceException("Encryption is only secure with a pre shared key. Please make sure PreSharedKey() returns a valid string.");
+                }
+                else
+                {
+                    psk = GetPreSharedKey();
+                }
+            }
+
+            //load the key and hash alghorithm providers
+            var symmetricKeyProvider = SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithmNames.AesCbcPkcs7);
+            var hashAlghorithmProvider = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Sha1);
+            //create a new hash object
+            CryptographicHash AEShash = hashAlghorithmProvider.CreateHash();
+
+            string encryptedString = "";
+
+            //try catch because of the chance of a too short PSK
+            try
+            {
+                //getting psk as byte array
+                byte[] pSkBytes = Encoding.UTF8.GetBytes(psk);
+                //declare a new hash byte array
+                byte[] hash = new byte[32];
+
+                //[] key must have a length of 32 bytes
+                //if pskBytes is long enough, use it
+                if (pSkBytes.Length >= 32)
+                {
+                    Array.Copy(pSkBytes, 0, hash, 0, 32);
+                }
+                //if pskbytes is too short, append it two times together 
+                //the CryptographicBuffer.CreateFromByteArray() method creates a 16 byte array even if you provide only a single letter/number as pre shared key
+                else
+                {
+                    AEShash.Append(CryptographicBuffer.CreateFromByteArray(Encoding.UTF8.GetBytes(psk)));
+
+                    byte[] temp;
+                    CryptographicBuffer.CopyToByteArray(AEShash.GetValueAndReset(), out temp);
+
+                    Array.Copy(temp, 0, hash, 0, 16);
+                    Array.Copy(temp, 0, hash, 15, 16);
+                }
+
+                //create the symmetric key that is used to encrypt the string from the hash bytes array
+                var AESkey = symmetricKeyProvider.CreateSymmetricKey(CryptographicBuffer.CreateFromByteArray(hash));
+
+                //create the IBuffer that is for the string
+                IBuffer buffer = CryptographicBuffer.CreateFromByteArray(Encoding.UTF8.GetBytes(text));
+
+                //encrypt the byte array with the symmetric key
+                encryptedString = CryptographicBuffer.EncodeToBase64String(CryptographicEngine.Encrypt(AESkey, buffer, null));
+
+                //return the Base64 string representation of the encrypted string byte array
+                return encryptedString;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+
+
+        /// <summary>
+        /// decyrpts an encrypted base64 string
+        /// </summary>
+        /// <param name="text">base64 string to decrypt</param>
+        /// <param name="psk">pre shared key used to decrypt</param>
+        /// <returns></returns>
+        public static string DecryptString(this string text, string psk = null)
+        {
+            //if no PSK is provided, load it from the PreSharedKey method. 
+            //remember to save this key securely between your apps or provide a key that is unique but available to both platforms!
+            if (string.IsNullOrEmpty(psk))
+            {
+                if (string.IsNullOrEmpty(GetPreSharedKey()))
+                {
+                    throw new NullReferenceException("Encryption is only secure with a pre shared key. Please make sure PreSharedKey() returns a valid string.");
+                }
+                else
+                {
+                    psk = GetPreSharedKey();
+                }
+            }
+
+            //load the key and hash alghorithm providers
+            var symmetricKeyProvider = SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithmNames.AesCbcPkcs7);
+            var hashAlghorithmProvider = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Sha1);
+            //create a new hash object
+            CryptographicHash AEShash = hashAlghorithmProvider.CreateHash();
+
+            string decryptedString = "";
+
+            //try catch because of the chance of a too short PSK
+            try
+            {
+                //getting psk as byte array
+                byte[] pSkBytes = Encoding.UTF8.GetBytes(psk);
+                //declare a new hash byte array
+                byte[] hash = new byte[32];
+
+                //[] key must have a length of 32 bytes
+                //if pskBytes is long enough, use it
+                if (pSkBytes.Length >= 32)
+                {
+                    Array.Copy(pSkBytes, 0, hash, 0, 32);
+                }
+                //if pskbytes is too short, append it two times together 
+                //the CryptographicBuffer.CreateFromByteArray() method creates a 16 byte array even if you provide only a single letter/number as pre shared key
+                else
+                {
+                    AEShash.Append(CryptographicBuffer.CreateFromByteArray(Encoding.UTF8.GetBytes(psk)));
+
+                    byte[] temp;
+                    CryptographicBuffer.CopyToByteArray(AEShash.GetValueAndReset(), out temp);
+
+                    Array.Copy(temp, 0, hash, 0, 16);
+                    Array.Copy(temp, 0, hash, 15, 16);
+                }
+
+                //create the symmetric key that is used to encrypt the string from the hash bytes array
+                var AESkey = symmetricKeyProvider.CreateSymmetricKey(CryptographicBuffer.CreateFromByteArray(hash));
+
+                //decode the input Base64 string
+                IBuffer buffer = CryptographicBuffer.DecodeFromBase64String(text);
+                //declare new byte array
+                byte[] dectryptedBytes;
+                //decrypt the IBuffer back to byte array
+                CryptographicBuffer.CopyToByteArray(CryptographicEngine.Decrypt(AESkey, buffer, null), out dectryptedBytes);
+                //get string back from the byte array
+                decryptedString = Encoding.UTF8.GetString(dectryptedBytes, 0, dectryptedBytes.Length);
+
+                //return plain text
+                return decryptedString;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+
+    }
+
+}
